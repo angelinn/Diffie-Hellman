0c830311c51e2bca3ccb55f03ed29528c75d974b
betrakiss <angelin.nedelchev@outlook.com>
2017-07-31 12:44:03 +0300

    Add test console app.

---
+++ src/ConsoleTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using DiffieHellman;
+using System;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        public static void PrintByteArray(byte[] bytes)
+        {
+            var sb = new StringBuilder("new byte[] { ");
+            foreach (var b in bytes)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            DiffieHellmanEncryption client = new DiffieHellmanEncryption();
+            Payload request = client.GenerateRequest();
+
+            DiffieHellmanEncryption server = new DiffieHellmanEncryption();
+            Payload response = server.GenerateResponse(request);
+
+            client.FinalizeKey(response);
+
+            PrintByteArray(client.Secret);
+            PrintByteArray(server.Secret);
+        }
+    }
+}
